05f05dac61a31cbde0e526f575ff82d4cf494b4b
Romika <romika2002chau.21@gmail.com>
2024-07-31 23:34:50 -0700

    Different page components has been created

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -1,9 +1,18 @@
-function App() {
+import { BrowserRouter, Routes, Route } from 
+"react-router-dom";
+import Product from './pages/Product';
+import Pricing from './pages/Pricing';
+import Homepage from './pages/Homepage';
 
+function App() {
   return (
-    <div>
-      Worldwise
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ worldwise/src/pages/Homepage.jsx
@@ -0,0 +1,7 @@
+function Homepage() {
+ return <div>
+    Home
+ </div>
+}
+
+export default Home
